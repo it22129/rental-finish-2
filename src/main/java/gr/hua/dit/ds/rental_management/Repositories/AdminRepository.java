06b8b63ea535addfb2882c2a165536330acb1b23
it22129 <92916088+it22129@users.noreply.github.com>
2025-02-19 15:20:54 +0200

    Delete src directory

--- src/main/java/gr/hua/dit/ds/rental_management/Repositories/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package gr.hua.dit.ds.rental_management.Repositories;
-
-import gr.hua.dit.ds.rental_management.Entities.Admin;
-import gr.hua.dit.ds.rental_management.Entities.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
-}
